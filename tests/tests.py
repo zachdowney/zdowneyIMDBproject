5f65c244df54ce2ecbbe55a5522638a39d423494
Zach Downey <zdowney@student.bridgew.edu>
2022-02-10 00:14:10 -0500

    fixed blank line at end

--- tests/tests.py
+++ tests/tests.py
@@ -10,7 +10,7 @@ def test_get_shows():
 # def test_show_db():
 #     show = {"items": [{"id": "tt5491994", "rank": "1", "title": "Planet Earth II",
 #                        "fullTitle": "Planet Earth II (2016)",
-#                       "year": "2016", "image": "https://m.media-amazon.com/images/M/MV5BZWYxODViMGYtMGE2ZC00ZGQ3LThhM \
+#                      "year": "2016", "image": "https://m.media-amazon.com/images/M/MV5BZWYxODViMGYtMGE2ZC00ZGQ3LThhM \
 #                        WUtYTVkNGE3OWU4NWRkL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyMjYwNDA2MDE@._V1_UX128_CR0,3,128,176_AL_.jpg",
 #                        "crew": "David Attenborough, Gordon Buchanan", "imDbRating": "9.5",
 #                        "imDbRatingCount": "110148"}]}
@@ -26,6 +26,4 @@ def test_get_shows():
 
 if __name__ == '__main__':
     test_get_shows()
- #   test_show_db()
-
 
