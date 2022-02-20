8b7839e5020e5459c91555d9331fe7af4e2da16f
Zach Downey <zdowney@student.bridgew.edu>
2022-02-19 22:50:46 -0500

    added in check for empty user ratings

--- sprintTwo.py
+++ sprintTwo.py
@@ -130,25 +130,37 @@ def fill_show_ratings_table(cursor: sqlite3.Cursor, data):
                 or data[i]['rank'] == '100' or data[i]['rank'] == '200':
             r = requests.get(user_url + secrets.apiKey + "/" + data[i]['id'])
             info = r.json()
-            cursor.execute('''INSERT INTO show_ratings (imDbId, totalRating, totalRatingVotes,
-            ten_rating_percentage, ten_rating_votes, nine_rating_percentage, nine_rating_votes,
-            eight_rating_percentage, eight_rating_votes, seven_rating_percentage, seven_rating_votes,
-            six_rating_percentage, six_rating_votes, five_rating_percentage, five_rating_votes, four_rating_percentage,
-            four_rating_votes, three_rating_percentage, three_rating_votes, two_rating_percentage, two_rating_votes,
-            one_rating_percentage, one_rating_votes)
-            VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''',
-                           (
-                               info['imDbId'], info['totalRating'], info['totalRatingVotes'],
-                               info['ratings'][0]['percent'], info['ratings'][0]['votes'],
-                               info['ratings'][1]['percent'], info['ratings'][1]['votes'],
-                               info['ratings'][2]['percent'], info['ratings'][2]['votes'],
-                               info['ratings'][3]['percent'], info['ratings'][3]['votes'],
-                               info['ratings'][4]['percent'], info['ratings'][4]['votes'],
-                               info['ratings'][5]['percent'], info['ratings'][5]['votes'],
-                               info['ratings'][6]['percent'], info['ratings'][6]['votes'],
-                               info['ratings'][7]['percent'], info['ratings'][7]['votes'],
-                               info['ratings'][8]['percent'], info['ratings'][8]['votes'],
-                               info['ratings'][9]['percent'], info['ratings'][9]['votes']))
+            if len(info['ratings']) == 0:
+                cursor.execute('''INSERT INTO show_ratings (imDbId, totalRating, totalRatingVotes,
+                            ten_rating_percentage, ten_rating_votes, nine_rating_percentage, nine_rating_votes,
+                            eight_rating_percentage, eight_rating_votes, seven_rating_percentage, seven_rating_votes,
+                            six_rating_percentage, six_rating_votes, five_rating_percentage, five_rating_votes, four_rating_percentage,
+                            four_rating_votes, three_rating_percentage, three_rating_votes, two_rating_percentage, two_rating_votes,
+                            one_rating_percentage, one_rating_votes)
+                            VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''',
+                               (
+                                   info['imDbId'], info['totalRating'], info['totalRatingVotes'],
+                                   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0))
+            else:
+                cursor.execute('''INSERT INTO show_ratings (imDbId, totalRating, totalRatingVotes,
+                ten_rating_percentage, ten_rating_votes, nine_rating_percentage, nine_rating_votes,
+                eight_rating_percentage, eight_rating_votes, seven_rating_percentage, seven_rating_votes,
+                six_rating_percentage, six_rating_votes, five_rating_percentage, five_rating_votes, four_rating_percentage,
+                four_rating_votes, three_rating_percentage, three_rating_votes, two_rating_percentage, two_rating_votes,
+                one_rating_percentage, one_rating_votes)
+                VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''',
+                               (
+                                   info['imDbId'], info['totalRating'], info['totalRatingVotes'],
+                                   info['ratings'][0]['percent'], info['ratings'][0]['votes'],
+                                   info['ratings'][1]['percent'], info['ratings'][1]['votes'],
+                                   info['ratings'][2]['percent'], info['ratings'][2]['votes'],
+                                   info['ratings'][3]['percent'], info['ratings'][3]['votes'],
+                                   info['ratings'][4]['percent'], info['ratings'][4]['votes'],
+                                   info['ratings'][5]['percent'], info['ratings'][5]['votes'],
+                                   info['ratings'][6]['percent'], info['ratings'][6]['votes'],
+                                   info['ratings'][7]['percent'], info['ratings'][7]['votes'],
+                                   info['ratings'][8]['percent'], info['ratings'][8]['votes'],
+                                   info['ratings'][9]['percent'], info['ratings'][9]['votes']))
 
     cursor.execute('SELECT * FROM shows where title is "Wheel of Time"')
     cursor.fetchone()
